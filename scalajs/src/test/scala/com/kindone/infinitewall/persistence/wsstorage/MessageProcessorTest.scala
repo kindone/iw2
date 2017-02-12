0e7442c1775b1fc4bc9fb88ca3bed8fd586cef2d
Won-Wook Hong <jradoo@gmail.com>
2017-02-12 11:41:47 +0900

    timer test

--- scalajs/src/test/scala/com/kindone/infinitewall/persistence/wsstorage/MessageProcessorTest.scala
+++ scalajs/src/test/scala/com/kindone/infinitewall/persistence/wsstorage/MessageProcessorTest.scala
@@ -8,20 +8,20 @@ import utest.framework.{ Test, Tree }
  */
 object MessageProcessorTest extends TestSuite {
   val tests: Tree[Test] = this{
-    'test1{
+    'testException{
       throw new Exception("test1")
     }
     'test2{
       1
     }
-    'test3{
+    'testDomTimer{
       val a = List[Byte](1, 2)
-      //      import scala.scalajs.js
-      //      import scala.scalajs.js.timers.SetTimeoutHandle
-      //      js.timers.setTimeout(1000) {
-      //        println("hello")
-      //      }
-      a(10)
+      import scala.scalajs.js
+      import scala.scalajs.js.timers.SetTimeoutHandle
+      js.timers.setTimeout(1000) {
+        println("hello")
+      }
+      //      a(10)
     }
   }
 }
